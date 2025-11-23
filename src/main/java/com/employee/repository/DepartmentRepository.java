6517da567d980664b8a7ec6a696f9bdf49f03cf0
Yuthmi Bangamu Arachchi <164158055+minanga2003@users.noreply.github.com>
2025-11-23 11:33:12 +0530

    remove unused classes and interfaces related to employee and department management

--- src/main/java/com/employee/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.repository;
-
-import com.employee.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
